81bc2da8c1da9edd5836c3381e93f1b7a9405d5c
sde100 <sde100@singlet.dev>
2023-02-02 23:28:17 +0800

    set animation tree as active on ready

--- characters/player.gd
+++ characters/player.gd
@@ -3,12 +3,14 @@ extends CharacterBody2D
 @export var SPEED = 150.0
 
 @onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
+@onready var animation_player = $AnimationPlayer
+@onready var state_machine = animation_tree["parameters/playback"]
 
 @onready var starting_direction = Vector2(0, 1)
 
 func _ready():
 	update_animation_parameters(starting_direction)
+	animation_tree.active = true
 
 func _physics_process(_delta):
 	var input_direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
@@ -31,8 +33,8 @@ func _physics_process(_delta):
 
 func update_animation_parameters(move_input : Vector2):
 	if (move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
 
 func pick_state():
 	print(velocity)
